943882b1c0bae0c655a6682bdcef93deafbd7509
Mark Sipiora <sipiora@UWAlumni.com>
2017-08-24 03:35:26 -0500

    Revised trash collector

---
+++ TrashPickup/TrashPickup/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashPickup.Migrations;
+using TrashPickup.Models;
+
+namespace TrashPickup.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (User.IsInRole("Customers"))
+            {
+                return RedirectToAction("Home", "Customers");
+            }
+            if (User.IsInRole("Employee"))
+            {
+                return RedirectToAction("Home", "Employee");
+            }
+            //List<SelectListItem> listItem = new List<SelectListItem>();
+            //DropDown drop = new DropDown();
+            ////ViewBag.DropDownValues = new SelectList(new[] { "First Value", "Second Value", "Third Value" });
+            //drop.Id = 1;
+            //drop.Value = "First Value";
+
+            //listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            //ViewBag.DropDownValues = new SelectList(listItem, "Text", "Value");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Select what day of the week you want your trash to be picked up";
+            List<SelectListItem> listItem = new List<SelectListItem>();
+            DropDown drop = new DropDown();
+            //ViewBag.DropDownValues = new SelectList(new[] { "First Value", "Second Value", "Third Value" });
+            drop.Id = 1;
+            drop.Value = "Sunday";
+
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 2;
+            drop.Value = "Monday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 3;
+            drop.Value = "Tuesday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 4;
+            drop.Value = "Wednesday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 5;
+            drop.Value = "Thursday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 6;
+            drop.Value = "Friday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            drop.Id = 7;
+            drop.Value = "Saturday";
+
+            listItem.Add(new SelectListItem() { Value = drop.Value, Text = drop.Id.ToString() });
+
+            ViewBag.DropDownValues = new SelectList(listItem, "Text", "Value");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Trash Pick-Up Schedule";
+
+            return View();
+        }
+    }
+}
---
+++ TrashPickup/TrashPickup/Models/SetWeeklySchedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    public class SetWeeklySchedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        [ForeignKey("Day")]
+        public int DayId { get; set; }
+        public Day Day { get; set; }
+    }
+}
+
+//public Application User {get; , set;}
+//[FOreignKey("User"]
+//public int UserId {get; set; }
+// public int ScheduleId { get; set; }
+//[ForeignKey("Week")]
+//public int week { get; set; }
+//public Week Week { get; set; }
